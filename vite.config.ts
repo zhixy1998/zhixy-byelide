e675233a15e0778e214244a5dc565132e566e2d4
zhixy1998 <nogiveup263@163.com>
2025-05-04 09:22:06 +0800

    feat: 🎸 再次解决页面白屏的问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/app/layout",
+  base:'/zhixy-byelide/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // 关键配置
